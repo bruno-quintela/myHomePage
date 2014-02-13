6e6e3e2237718201da570faf747373a03aadc0d2
bruno-quintela <bruno.quintela.bastos@gmail.com>
2014-02-13 00:30:56 +0000

    first steps

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+    initUI();
+});
 
+function initUI() {
+    setElemScreenHeight('section1');
+}
+
+function setElemScreenHeight(elemId) {
+    $('#' + elemId).css('height', $(document).height());
+}
